fd2975c4998c321a6b18855947e09cec8ae4414f
orion-cirb <pf-microscopie.cirb@college-de-france.fr>
2022-03-28 10:40:52 +0200

    fix coloc

--- src/main/java/MNP9_NeuN.java
+++ src/main/java/MNP9_NeuN.java
@@ -12,7 +12,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
